3eb3ac67e7695750907797780667831249740b6b
Garrin Shieh <garrin.shieh@gmail.com>
2023-03-30 17:07:08 -0400

    Created NewsAPI call. May use a different API.

--- src/api_calls.py
+++ src/api_calls.py
@@ -1,6 +1,7 @@
 from alpaca.data import StockBarsRequest
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.timeframe import TimeFrame
+from newsapi import NewsApiClient
 import config
 
 
@@ -17,4 +18,12 @@ def call_alpaca(symbols: [], timeframe: TimeFrame, start: str, end: str):
         end=end
     )
     call = StockHistoricalDataClient.get_stock_bars(api, request_params)
+    return call
+
+
+def call_newsapi(symbol: str, start: str, end: str):
+    # ... code to call newsapi api
+    api_key = config.newsapi_api_key
+    api = NewsApiClient(api_key=api_key)
+    call = api.get_everything(q=symbol, from_param=start, to=end, language='en', sort_by='relevancy')
     return call
