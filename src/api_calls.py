6037ce81f8ed070eddf998b57cc8eede07f70eac
Garrin Shieh <garrin.shieh@gmail.com>
2023-04-02 18:29:59 -0400

    Added API calls + examples for the following:
    Company Overview
    Income Statement
    Balance Sheet
    Cash Flow
    Earnings

--- src/api_calls.py
+++ src/api_calls.py
@@ -5,7 +5,7 @@ api_key = config.alphavantage_api_key
 api_url = 'https://www.alphavantage.co/query'
 
 
-def call_alpha_vantage_daily(symbol: str):
+def call_daily(symbol: str):
     # ... code to call alphavantage api
     url = api_url + '?function=TIME_SERIES_DAILY_ADJUSTED' + \
           '&symbol=' + symbol + \
@@ -17,12 +17,67 @@ def call_alpha_vantage_daily(symbol: str):
     return data
 
 
-def call_alpha_vantage_news(symbol: str):
+def call_news(symbol: str):
     # ... code to call alphavantage api
     url = api_url + '?function=NEWS_SENTIMENT' + \
           '&tickers=' + symbol + \
           '&apikey=' + api_key + \
           '&limit=200'
+    r = requests.get(url)
+    data = r.json()
+
+    return data
+
+
+def call_company_overview(symbol: str):
+    # ... code to call alphavantage api
+    url = api_url + '?function=OVERVIEW' + \
+          '&symbol=' + symbol + \
+          '&apikey=' + api_key
+    r = requests.get(url)
+    data = r.json()
+
+    return data
+
+
+def call_income_statement(symbol: str):
+    # ... code to call alphavantage api
+    url = api_url + '?function=INCOME_STATEMENT' + \
+          '&symbol=' + symbol + \
+          '&apikey=' + api_key
+    r = requests.get(url)
+    data = r.json()
+
+    return data
+
+
+def call_balance_sheet(symbol: str):
+    # ... code to call alphavantage api
+    url = api_url + '?function=BALANCE_SHEET' + \
+          '&symbol=' + symbol + \
+          '&apikey=' + api_key
+    r = requests.get(url)
+    data = r.json()
+
+    return data
+
+
+def call_cash_flow(symbol: str):
+    # ... code to call alphavantage api
+    url = api_url + '?function=CASH_FLOW' + \
+          '&symbol=' + symbol + \
+          '&apikey=' + api_key
+    r = requests.get(url)
+    data = r.json()
+
+    return data
+
+
+def call_earnings(symbol: str):
+    # ... code to call alphavantage api
+    url = api_url + '?function=EARNINGS' + \
+          '&symbol=' + symbol + \
+          '&apikey=' + api_key
     r = requests.get(url)
     data = r.json()
 
