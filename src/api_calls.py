2166f4ee8212ca7a27567db5f2c80f768280efc0
Garrin Shieh <garrin.shieh@gmail.com>
2023-04-03 14:09:14 -0400

    Edited Daily API call, added some ML algorithms from HW3. Need to fix.

--- src/api_calls.py
+++ src/api_calls.py
@@ -5,24 +5,24 @@ api_key = config.alphavantage_api_key
 api_url = 'https://www.alphavantage.co/query'
 
 
-def call_daily(symbol: str):
+def call_daily(symbol: str, outputsize: str = 'compact'):
     # ... code to call alphavantage api
     url = api_url + '?function=TIME_SERIES_DAILY_ADJUSTED' + \
           '&symbol=' + symbol + \
           '&apikey=' + api_key + \
-          '&outputsize=compact'
+          '&outputsize=' + outputsize
     r = requests.get(url)
     data = r.json()
 
     return data
 
 
-def call_news(symbol: str):
+def call_news(symbol: str, limit: int = 200):
     # ... code to call alphavantage api
     url = api_url + '?function=NEWS_SENTIMENT' + \
           '&tickers=' + symbol + \
           '&apikey=' + api_key + \
-          '&limit=200'
+          '&limit=' + limit
     r = requests.get(url)
     data = r.json()
 
