c198d117522a2335f9b905276db8b4fdade55671
itsGarrin <garrin.shieh@gmail.com>
2023-04-05 01:01:31 -0400

    Finished creating S&P500 testing set, will finish training set later.

--- src/api_calls.py
+++ src/api_calls.py
@@ -1,4 +1,6 @@
+import csv
 import requests
+
 import config
 
 api_key = config.alphavantage_api_key
@@ -7,37 +9,46 @@ api_url = 'https://www.alphavantage.co/query'
 
 def call_daily(symbol: str, outputsize: str = 'compact'):
     # ... code to call alphavantage api
-    url = api_url + '?function=TIME_SERIES_DAILY_ADJUSTED' + \
-          '&symbol=' + symbol + \
-          '&apikey=' + api_key + \
-          '&outputsize=' + outputsize
-    r = requests.get(url)
-    data = r.json()
+    try:
+        url = api_url + '?function=TIME_SERIES_DAILY_ADJUSTED' + \
+              '&symbol=' + symbol + \
+              '&apikey=' + api_key + \
+              '&outputsize=' + outputsize
+        r = requests.get(url)
+        data = r.json()
 
-    return data
+        return data
+    except:
+        return None
 
 
 def call_news(symbol: str, limit: int = 200):
     # ... code to call alphavantage api
-    url = api_url + '?function=NEWS_SENTIMENT' + \
-          '&tickers=' + symbol + \
-          '&apikey=' + api_key + \
-          '&limit=' + str(limit)
-    r = requests.get(url)
-    data = r.json()
+    try:
+        url = api_url + '?function=NEWS_SENTIMENT' + \
+              '&tickers=' + symbol + \
+              '&apikey=' + api_key + \
+              '&limit=' + str(limit)
+        r = requests.get(url)
+        data = r.json()
 
-    return data
+        return data
+    except:
+        return None
 
 
 def call_company_overview(symbol: str):
     # ... code to call alphavantage api
-    url = api_url + '?function=OVERVIEW' + \
-          '&symbol=' + symbol + \
-          '&apikey=' + api_key
-    r = requests.get(url)
-    data = r.json()
+    try:
+        url = api_url + '?function=OVERVIEW' + \
+              '&symbol=' + symbol + \
+              '&apikey=' + api_key
+        r = requests.get(url)
+        data = r.json()
 
-    return data
+        return data
+    except:
+        return None
 
 
 def call_income_statement(symbol: str):
@@ -71,6 +82,18 @@ def call_cash_flow(symbol: str):
     data = r.json()
 
     return data
+
+
+def call_listings():
+    # ... code to call alphavantage api
+    url = api_url + '?function=LISTING_STATUS' + \
+          '&apikey=' + api_key
+    r = requests.get(url)
+    decoded_content = r.content.decode('utf-8')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+
+    return my_list
 
 
 def call_earnings(symbol: str):
